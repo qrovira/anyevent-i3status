19bb13a86c6901d5d3577b8ee5a83abe8653c4b1
Quim Rovira <joaquim.rovira@booking.com>
2014-05-21 21:30:01 +0200

    Support also Linux::Sysfs for battery stats instead of just Apm.
    Linux::Sysfs has potential to remove some of the crap i quickly
    put together first (like even thermal / sensors).
    Too bad it blows up in all tests due to the 'once' warnings, i
    will try to take a look and fix..

--- lib/AnyEvent/I3Status/Plugin/Battery.pm
+++ lib/AnyEvent/I3Status/Plugin/Battery.pm
@@ -6,6 +6,7 @@ use warnings;
 
 use JSON;
 use Sys::Apm;
+use Linux::Sysfs;
 
  
 sub register {
@@ -15,33 +16,67 @@ sub register {
         heartbeat => $opts{prio} => sub {
             my ($i3status, $status) = @_;
 
-            my $apm = Sys::Apm->new
-                or return;
+            if( my $apm = Sys::Apm->new ) {
+                my $s = { name => "battery" };
 
-            $apm->battery_status == 4
-                or return;
+                if( $apm->battery_status == 3 ) {
+                    $s->{fulltext} = $apm->charge."%";
+                }
+                else {
+                    $s->{fulltext} = $apm->charge."% ".
+                        $apm->remaining.$apm->units;
+                    # Charge-dependant color
+                    $s->{color} = '#ffa500' if $apm->charge < 50;
+                    $s->{color} = '#ff0000' if $apm->charge < 20;
+                    # Critical status
+                    if( $apm->battery_status == 2 ) {
+                        $s->{urgent} = JSON::true;
+                        $s->{color} = '#ff0000';
+                    }
+                }
 
-            my $s = {
-                name => "apm",
-                $apm->battery_status == 2 ? ( urgent => JSON::true ) : ()
-            };
-
-            if( $apm->battery_status == 3 ) {
-                $s->{full_text} = $apm->charge."%";
+                push @$status, $s;
             }
-            else {
-                $s->{full_text} = $apm->charge."% ".$apm->remaining.$apm->units;
+            elsif( my $power_supply = Linux::Sysfs::Class->open('power_supply') ) {
+                my @all;
+                foreach my $dev ( $power_supply->get_devices ) {
+                    my $type = attrval($dev->get_attr('type'));
+                    if( $type eq 'Battery' ) {
+                        my $status = attrval($dev->get_attr('status'));
+                        my ($full,$now,$rate) = map { attrval($dev->get_attr($_)) }
+                            qw/ energy_full energy_now power_now /;
+                        my $percent = sprintf( "%.1f", 100 * $now / ( $full // 1 ) );
+                        my $time = ( $status eq 'Charging' ? ($full - $now) : $now ) / $rate;
+                        my $ftime = sprintf("%d:%02d", int $time, 60 * ($time - int $time));
 
-                $s->{color} = '#ffa500'
-                    if $apm->charge < 50;
-
-                $s->{color} = '#ff0000'
-                    if $apm->charge < 20;
+                        unshift @all, {
+                            name => "battery",
+                            instance => $dev->name,
+                            full_text => "$percent\% ($ftime left)",
+                            ( $percent < 50 ? ( color => '#ffa500' ) : () ),
+                            ( $percent < 20 ? ( color => '#ff0000' ) : () ),
+                            ( $dev->get_attr('alarm') ? ( urgent => JSON::true ) : () ),
+                        };
+                    }
+                    elsif( $type eq 'Mains' ) {
+                        push @all, { name => "battery", instance => "AC", full_text => "AC" }
+                            if attrval($dev->get_attr('online'));
+                    }
+                }
+                push @$status, @all;
             }
-
-            push @$status, $s;
         }
     );
+}
+
+# wtf.. i guess this makes more sense if we were to keep attrs around
+sub attrval {
+    my ($attr) = @_;
+    $attr->can_read || return;
+    $attr->read;
+    my $val = $attr->value;
+    chomp $val;
+    return $val;
 }
 
 1;
