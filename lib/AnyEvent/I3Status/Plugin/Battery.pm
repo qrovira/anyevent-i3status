f808192efd6278663bb1b3e0bc40ed25548672cf
Quim Rovira <quim@rovira.cat>
2015-06-08 00:00:03 +0200

    Update Battery plugin to the new plugin approach, and try to uniformize APM and SysFS plugins a bit.. :/

--- lib/AnyEvent/I3Status/Plugin/Battery.pm
+++ lib/AnyEvent/I3Status/Plugin/Battery.pm
@@ -1,5 +1,7 @@
 package AnyEvent::I3Status::Plugin::Battery;
 
+use parent 'AnyEvent::I3Status::Plugin';
+
 use 5.014;
 use strict;
 use warnings;
@@ -10,89 +12,175 @@ use Sys::Apm;
 use Linux::Sysfs;
 
  
-sub register {
-    my ($class, $i3status, %opts) = @_;
+=head1 NAME
 
-    my $flop = 0;
-    my @FLOPS = ('#ff0000','#ffff00');
-    my $long = {};
+AnyEvent::I3Status::Plugin::Battery - Display battery status
 
-    $i3status->reg_cb(
-        heartbeat => $opts{prio} => sub {
-            my ($i3status, $status) = @_;
+=head1 SYNOPSIS
 
-            if( my $apm = Sys::Apm->new ) {
-                my $s = { name => "battery" };
-
-                if( $apm->battery_status == 3 ) {
-                    $s->{fulltext} = $apm->charge."%";
-                }
-                else {
-                    $s->{fulltext} = $apm->charge."% ".
-                        $apm->remaining.$apm->units;
-                    # Charge-dependant color
-                    $s->{color} = '#ffa500' if $apm->charge < 50;
-                    $s->{color} = '#ff0000' if $apm->charge < 20;
-                    # Critical status
-                    if( $apm->battery_status == 2 ) {
-                        $s->{urgent} = JSON::true;
-                        $s->{color} = '#ff0000';
-                    }
-                }
-
-                push @$status, $s;
-            }
-            elsif( my $power_supply = Linux::Sysfs::Class->open('power_supply') ) {
-                my @all;
-                foreach my $dev ( $power_supply->get_devices ) {
-                    my $type = attrval($dev, 'type');
-                    if( $type eq 'Battery' ) {
-                        my $status = attrval($dev, 'status');
-                        my $full = attrval($dev, 'energy_full') // attrval($dev, 'charge_full');
-                        my $now = attrval($dev, 'energy_now') // attrval($dev, 'charge_now');
-                        my $rate = attrval($dev, 'power_now') // attrval($dev, 'current_now');
-                        my $alarm = attrval($dev, 'alarm');
-
-                        my $percent = sprintf( "%.1f", 100 * $now / ( $full || 1 ) );
-                        my $time =
-                            $rate && $status eq 'Charging' ? ($full - $now) / $rate :
-                            $rate && $status eq 'Discharging' ? $now / $rate :
-                            0;
-                        my $ftime = sprintf("%d:%02d", int $time, 60 * ($time - int $time));
-
-
-                        unshift @all, {
-                            name => "battery",
-                            instance => $dev->name,
-                            full_text => "⚡ $percent\%" . ($long->{$dev->name} ? " ($ftime left)" : ""),
-                            ( $status eq 'Full' ? ( color => '#00ff00' ) : () ),
-                            ( $percent < 50 ? ( color => '#ffa500' ) : () ),
-                            ( $percent < 20 ? ( color => '#ff0000' ) : () ),
-                            ( $percent < 10 ? ( color => $FLOPS[$flop = ($flop+1) % 2] ) : () ),
-                            ( $alarm ? ( urgent => JSON::true ) : () ),
-                        };
-                    }
-                    elsif( $type eq 'Mains' ) {
-                        push @all, {
-                            name => "battery",
-                            instance => "AC",
-                            full_text => "⚡ AC"
-                        } if attrval($dev,'online');
-                    }
-                }
-                push @$status, @all;
-            }
+    Battery => {
+        colors  => {
+            full => '#ffffff',
+            ok => '#ff0000',
+            half => '#ff4400',
+            warning => '#ffa500',
+            critical => ['#ff0000', '#ffff00'], # cycle colors to call for attention
         },
-        click => sub {
-            my ($i3status, $click) = @_;
+        long         => 0,
+    }
 
-            use Data::Dumper;
-            warn Dumper $click;
+=head1 OPTIONS
 
-            $long->{$click->{instance}} = !$long->{$click->{instance}}
-                if( $click->{name} eq 'battery' );
-        }
+=over
+
+=item colors
+
+You can specify colors for any of the 'full', 'ok', 'half' (<50%), 'warning' (<25%) and critical (<10%) statuses.
+
+If an array is provided, colors will be cycled on every heartbeat.
+
+=item long
+
+Long format displays time left
+
+=back
+
+=head2 Click handlers
+
+You can switch between long and short format clicking on the status message.
+
+=cut
+
+sub new {
+    my ($class, %opts) = @_;
+    my $self = $class->SUPER::new(
+        colors => {
+            ok => '#00ff00',
+            half => '#ff4400',
+            warning => '#ffa500',
+            critical => [ '#ff0000', '#ffff00' ],
+        },
+        %opts
     );
+
+    if( my $apm = Sys::Apm->new ) {
+        $self->{_apm} = $apm;
+    }
+    elsif( my $power_supply = Linux::Sysfs::Class->open('power_supply') ) {
+        # We could check devices and keep them around already,
+        # but I'm unsure if there can be any changes (eg. battery/docks/etc.),
+        # and I'm not concerned about performance at this point..
+        $self->{_sysfs} = $power_supply;
+    }
+
+    return $self;
+}
+
+sub status {
+    my $self = shift;
+    my @sources = $self->{_apm} ?
+        $self->_sources_apm :
+        $self->{_sysfs} ?
+        $self->_sources_sysfs : ();
+
+    return {
+        full_text => "No battery info",
+        urgent => 1,
+    } unless @sources;
+
+    return map {
+        $_->{type} eq 'main' && $_->{status} ? 
+            {
+                name => "battery",
+                instance => "AC",
+                full_text => "⚡ AC"
+            } :
+        $_->{type} eq 'battery' ?
+            {
+                name => "battery",
+                instance => $_->{name},
+                full_text => "⚡ $_->{charge}\%" .
+                    (($self->{long}{$_->{name}} // $self->{long}) ? " ($_->{left} left)" : ""),
+                (
+                    $_->{charge} < 10 ? $self->_color('critical') :
+                    $_->{charge} < 25 ? $self->_color('warning') :
+                    $_->{charge} < 50 ? $self->_color('half') :
+                    $_->{status} eq 'full' ? $self->_color('full') :
+                    $self->_color('ok')
+                ),
+                ( $_->{alarm} ? ( urgent => JSON::true ) : () ),
+            } : ()
+    } @sources;
+}
+
+sub _sources_apm {
+    my $self = shift;
+    my $apm = $self->{_apm};
+    my @sources;
+
+    push @sources, {
+        type => "battery",
+        status => $apm->ac_status == 1
+    };
+
+    push @sources, {
+        type => "battery",
+        name => "APM",
+        left => $apm->remaining.$apm->units,
+        charge => $apm->charge,
+        status => $apm->status == 3 ? 'charging' : $apm->{charge} > 95 ? 'full' : 'discharging' ,
+        alarm => $apm->status == 2,
+    };
+
+    return @sources;
+}
+
+sub _sources_sysfs {
+    my $self = shift;
+    my $power_supply = $self->{_sysfs};
+    my @sources;
+
+    foreach my $dev ( $power_supply->get_devices ) {
+        my $type = attrval($dev, 'type');
+        if( $type eq 'Battery' ) {
+            my $status = attrval($dev, 'status');
+            my $full = attrval($dev, 'energy_full') // attrval($dev, 'charge_full');
+            my $now = attrval($dev, 'energy_now') // attrval($dev, 'charge_now');
+            my $rate = attrval($dev, 'power_now') // attrval($dev, 'current_now');
+            my $alarm = attrval($dev, 'alarm');
+
+            my $charge = sprintf( "%.1f", 100 * $now / ( $full || 1 ) );
+            my $time =
+                $rate && $status eq 'Charging' ? ($full - $now) / $rate :
+                $rate && $status eq 'Discharging' ? $now / $rate :
+                0;
+            my $ftime = sprintf("%d:%02d", int $time, 60 * ($time - int $time));
+
+
+            push @sources, {
+                type => "battery",
+                name => $dev->name,
+                charge => $charge,
+                left => $ftime,
+                status => lc($status),
+                alarm => $alarm,
+            }
+        }
+        elsif( $type eq 'Mains' ) {
+            push @sources, {
+                type => "battery",
+                status => attrval($dev, 'online')
+            };
+        }
+    }
+
+    return @sources;
+}
+
+sub click {
+    my ($self, $click) = @_;
+
+    $self->{long} = !$self->{long};
 }
 
 # wtf.. i guess this makes more sense if we were to keep attrs around
@@ -108,6 +196,22 @@ sub attrval {
     chomp $val;
 
     return $val;
+}
+
+sub _color {
+    my $self = shift;
+    my $color = shift;
+
+    return () unless $self->{colors}{$color};
+
+    return ( color => $self->{color}{$color} )
+        unless ref $self->{color}{$color};
+
+    my $current = $self->{_color_flops}{$color} =
+        ( $self->{_color_flops}{$color} + 1 ) %
+        scalar( @{$self->{colors}{$color}} );
+
+    return ( color => $self->{color}{$color}[$current] );
 }
 
 1;
