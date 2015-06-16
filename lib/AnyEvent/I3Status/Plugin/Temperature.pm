8f5c0dea3b74733025103a94b21257952137a779
Quim Rovira <quim@rovira.cat>
2015-06-16 22:38:57 +0200

    Update Temperature plugin

--- lib/AnyEvent/I3Status/Plugin/Temperature.pm
+++ lib/AnyEvent/I3Status/Plugin/Temperature.pm
@@ -1,49 +1,81 @@
 package AnyEvent::I3Status::Plugin::Temperature;
 
+use parent 'AnyEvent::I3Status::Plugin';
+
 use 5.014;
 use strict;
 use warnings;
-
 use utf8;
 
 use JSON;
 
+=head1 NAME
+
+AnyEvent::I3Status::Plugin::Temperature - Display system temperatures
+
+=head1 SYNOPSIS
+
+    Temperature => {
+        sensors => "warn"
+        hide_high => 0,
+    }
+
+=head1 OPTIONS
+
+=over
+
+=item sensors
+
+Which sensors to display.
+
+Can be a comma-separated list of sensors names, "crit" to display sensors over the critical limit, or "warn" to display sensors over the warning limit.
+
+=item hide_high
+
+Hide the high temperature limit.
+
+=back
+
+=cut
+
  
-sub register {
-    my ($class, $i3status, %opts) = @_;
-
-    $i3status->reg_cb(
-        heartbeat => $opts{prio} => sub {
-            my ($i3status, $status) = @_;
-
-            my %sensors = parse_sensors();
-
-            if( exists $opts{sensor} and $opts{sensor} eq 'all' ) {
-                push @$status, sensor_status( $sensors{ $_ }, %opts )
-                    foreach( keys %sensors );
-            }
-            elsif( exists $opts{sensor} ) {
-                push @$status, sensor_status( $sensors{ $opts{sensor} }, %opts );
-            }
-            else {
-                my ( $highest ) = sort {
-                    $sensors{$b}{temp} <=>  $sensors{$a}{temp}
-                } keys %sensors;
-
-                push @$status, sensor_status( $sensors{$highest}, %opts );
-            }
-        }
+sub new {
+    my ($class, %opts) = @_;
+    my $self = $class->SUPER::new(
+        sensors   => "warn",
+        hide_high => 0,
+        %opts
     );
+
+    return $self;
+}
+
+sub status {
+    my ($self) = @_;
+    my %sensors = _parse_sensors();
+
+    return
+        map { $self->sensor_status( $sensors{ $_ } ) }
+        sort { $a cmp $b } 
+        (
+            $self->{sensors} eq 'all' ?
+            keys %sensors :
+            $self->{sensors} eq 'warn' ?
+            grep { $sensors{ $_ }{temp} >= $sensors{ $_ }{high} } keys %sensors :
+            $self->{sensors} eq 'crit' ?
+            grep { $sensors{ $_ }{temp} >= $sensors{ $_ }{crit} } keys %sensors :
+            split ',', $self->{sensors}
+        );
 }
 
 sub sensor_status {
-    my ($sensor, %opts) = @_;
+    my ($self,$sensor) = @_;
 
     return {
         name => "temperature",
         instance => $sensor->{name},
         full_text => $sensor->{temp}."°".$sensor->{unit}.
-            ( (defined($sensor->{high}) && !$opts{no_high}) ?
+            ( (defined($sensor->{high}) && !$self->{hide_high}) ?
                 '/'.$sensor->{high}."°".$sensor->{unit} : '' ),
         ( defined($sensor->{high}) && $sensor->{temp} > $sensor->{high} ?
             (color => '#ff0000', urgent => JSON::true) : ()
@@ -52,7 +84,7 @@ sub sensor_status {
 
 }
 
-sub parse_sensors {
+sub _parse_sensors {
     return map {
         /^ (?<name>[^:]+): \s+
             \+(?<temp>[\d\.]+)\302\260(?<unit>[CF]) \s+
@@ -62,7 +94,7 @@ sub parse_sensors {
                     crit\s=\s\+(?<crit>[\d\.]+)\302\260[CF]
                 \)
             )?
-        /x ? ( $+{name} => +{ %+ } ) : ()
+        /x ? ( $+{name} => +{ high => $+{crit}, %+ } ) : ()
     } split "\n", `sensors`;
 }
 
