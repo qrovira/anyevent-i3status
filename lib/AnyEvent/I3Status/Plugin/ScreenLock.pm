f885adc804f1715ddda3fa5d3475418767d18fd5
Quim Rovira <joaquim.rovira@booking.com>
2015-06-19 20:06:01 +0200

    Small improvements to the ScreenLock plugin

--- lib/AnyEvent/I3Status/Plugin/ScreenLock.pm
+++ lib/AnyEvent/I3Status/Plugin/ScreenLock.pm
@@ -38,6 +38,10 @@ AnyEvent::I3Status::Plugin::ScreenLock - Control screen locking
 
 Timeout for an idle session to trigger the screen lock
 
+=item countdown
+
+Show remaining time before locking
+
 =item autolock
 
 Whether autolock is enabled or not
@@ -62,40 +66,31 @@ You can click on the status to disable autolock
 sub new {
     my ($class, %opts) = @_;
     my $self = $class->SUPER::new(
-        timeout  => 120,
-        autolock => 1,
-        image    => undef,
-        color    => '000000',
+        timeout   => 120,
+        countdown => 0,
+        autolock  => 1,
+        image     => undef,
+        color     => '000000',
         %opts
     );
+
+    $self->set_timer;
 
     return $self;
 }
 
 sub status {
     my ($self) = @_;
-    my $time = GetIdleTime;
 
     #say STDERR "PID $lock_cv, time $time, timeout is $timeout, autolock is $autolock";
 
-    if( !$self->{lock_cv} && $self->{autolock} && $time > $self->{timeout} ) {
-        $self->{lock_cv} = run_cmd(
-            [
-                "i3lock",
-                "-n",
-                ( $self->{image} ? ("-i", $self->{image}) : "" ),
-                ( $self->{color} ? ("-c", $self->{color}) : "" )
-            ],
-        );
-    }
-
     return {
         name => "screenlock",
-        full_text => "⌛ $self->{timeout}",
+        full_text => ($self->{countdown} ? sprintf("⌛ %03d", $self->{time_left}) : "⌛"),
         short_text => "⌛",
         color => (
             $self->{autolock} ?
-                ($time > $self->{timeout} - 10) ?
+                ($self->{time_left} < 20) ?
                 '#ffff00' :
                 '#00ff00' :
                 '#ffffff'
@@ -106,7 +101,55 @@ sub status {
 sub click {
     my ($self, $click) = @_;
 
-    $self->{autolock} = !$self->{autolock};
+    if( $click->{button} == 1 ) {
+        $self->{autolock} = !$self->{autolock};
+    }
+    elsif( $click->{button} == 3 ) {
+        $self->lock;
+    }
+}
+
+sub set_timer {
+    my $self = shift;
+
+    $self->{time_left} = $self->{timeout};
+
+    $self->{check_lock_cv} = AnyEvent->timer(
+        interval => 0.5,
+        cb => sub {
+            my $time = GetIdleTime;
+
+            $self->{time_left} = $self->{timeout} - $time
+                if $self->{time_left} > 0;
+
+            return unless $self->{autolock} && $self->{time_left} <= 0;
+
+            $self->lock;
+        }
+    );
+}
+
+sub lock {
+    my $self = shift;
+
+    # last i3lock command did not return yet
+    return if $self->{i3lock_cv};
+
+    $self->{i3lock_cv} = run_cmd(
+        [
+            "i3lock",
+            "-n",
+            ( $self->{image} ? ("-i", $self->{image}) : "" ),
+            ( $self->{color} ? ("-c", $self->{color}) : "" )
+        ],
+    );
+
+    $self->{i3lock_cv}->cb( sub {
+        my $status = shift->recv;
+        $self->{i3lock_cv} = undef;
+
+        $self->{time_left} = $self->{timeout};
+    } );
 }
 
 1;
